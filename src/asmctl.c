e3cc4d8bf6efc2e678cfb3417aaadddef478b204
Yuichiro NAITO <naito.yuichiro@gmail.com>
2015-08-26 19:40:30 +0900

    Add License

--- src/asmctl.c
+++ src/asmctl.c
@@ -1,3 +1,40 @@
+/*-
+ * Copyright (c) 2015 Yuichiro NAITO <naito.yuichiro@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Command line tool for Apple's System Management Console (SMC).
+ *
+ * This command requires following sysctl variables.
+ *
+ *  hw.acpi.video.lcd0.*    (acpi_video(4))
+ *  hw.asmc.0.light.control (asmc(4))
+ *  hw.acpi.acline          (acpi(4))
+ */
+
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <stdio.h>
@@ -14,10 +51,16 @@
 
 #define AC_POWER "hw.acpi.acline"
 
+/* Keyboard backlight current level (0-100) */
 int kb_current_level;
 
+/* Number of video levels */
 int num_of_video_levels=0;
+
+/* Array of video level values */
 int *video_levels=NULL;
+
+/* Video backlight current level (one of video_level)*/
 int video_current_level;
 
 /* set 1 if AC powered else 0 */
