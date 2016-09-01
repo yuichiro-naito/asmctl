065285c5c0aa013bd51890b87f1f7dd953d9125a
Yuichiro NAITO <naito.yuichiro@gmail.com>
2016-09-01 18:00:49 +0900

    use BINDIR

--- devd/asmctl.conf.s
+++ devd/asmctl.conf.s
@@ -2,5 +2,5 @@ notify 20 {
 	match "system"		"ACPI";
 	match "subsystem"	"ACAD";
 	action "/etc/rc.d/power_profile $notify";
-	action "%%EXEC_PREFIX%%/asmctl video acpi";
+	action "%%BINDIR%%/asmctl video acpi";
 };
