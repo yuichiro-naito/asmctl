925c0ef6a75bf9c43e47cff5bc811b55c06e9bb2
Joshua Rogers <Joshua@Joshua.Hu>
2024-09-25 18:52:38 +0200

    Update license

--- src/asmctl.c
+++ src/asmctl.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2015 Yuichiro NAITO <naito.yuichiro@gmail.com>
+ * Copyright (c) 2024 Joshua Rogers <joshua@joshua.hu>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
